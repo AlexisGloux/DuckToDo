40e3a8ded651bef34955861357a254b9e653b96b
Gloux Alexis <gloux10@gmail.com>
2020-02-16 13:35:20 +0100

    before read db (permission denied)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD0GOr4AturWXxxzWQm6Ma1jSosCFZuJu8',
+  authDomain: 'ducktodo-ionic.firebaseapp.com',
+  databaseURL: 'https://ducktodo-ionic.firebaseio.com',
+  projectId: 'ducktodo-ionic',
+  storageBucket: 'ducktodo-ionic.appspot.com',
+  messagingSenderId: '24615641978',
+  appId: '1:24615641978:web:d394167c12fe675873ecc3'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
