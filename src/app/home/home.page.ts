d4790767c3342b8d6108a926845fb5ad885e4b9b
Gloux Alexis <gloux10@gmail.com>
2020-02-16 14:15:31 +0100

    ToDo list done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,11 +10,13 @@ export class HomePage {
   currentDate: string;
   myTask: string;
   addTask: boolean;
+  tasks = [];
 
   constructor(public afDB: AngularFireDatabase) {
     const date = new Date();
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
     this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
   }
 
   addTaskToFirebase() {
@@ -29,5 +31,28 @@ export class HomePage {
   showForm() {
     this.addTask = !this.addTask;
     this.myTask = '';
+  }
+
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
+  changeCheckState(t: any) {
+    console.log('checked: ' + t.checked);
+    this.afDB.object('Tasks/' + t.key + '/checked/').set(t.checked);
+  }
+
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
   }
 }
